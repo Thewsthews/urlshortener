c0df8fa3bbf710eadcf05097367d9702c69e9801
Thewsthews <etiegnim@gmail.com>
2025-02-12 14:40:03 +0300

    Update server main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"math/rand"
+	"net/http"
+	"sync"
 	"time"
 )
 
@@ -10,7 +12,7 @@ const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 var store = struct {
 	m map[string]string
-	sunc.RWMutex
+	sync.RWMutex
 }{m: make(map[string]string)}
 
 func generateShortURL() string {
@@ -20,4 +22,16 @@ func generateShortURL() string {
 		shortURL[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(shortURL)
+}
+
+type ShortenRequest struct {
+	LongURL string `json:"long_url"`
+}
+
+type ShortenResponse struct {
+	ShortURL string `json:"short_url"`
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	var req ShortenRequest
 }
